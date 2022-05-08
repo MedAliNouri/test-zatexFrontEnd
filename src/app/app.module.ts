73d5b625b96674f1e57d1338fc571ce4290b969b
MedALiNouri <mohamedalinouri.wrk@gmail.com>
2022-05-08 22:07:55 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './component/auth/auth.module';
+import { DashboardModule } from './component/dashboard/dashboard.module';
+import { ContentLayoutComponent } from './shared/component/content-layout/content-layout.component';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+  
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule,
+    DashboardModule,
+    SharedModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
